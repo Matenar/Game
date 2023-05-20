d4e88053b329980fdb258c75866fa9e2a7e612b9
Matenar <123372371+Matenar@users.noreply.github.com>
2023-05-20 19:28:31 +0200

    Add files via upload

---
+++ src/entity/Player.java
@@ -0,0 +1,205 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+import main.UtilityTool;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Player extends Entity {
+    GamePanel gp;
+    KeyHandler keyH;
+
+    public final int screenX;
+    public final int screenY;
+    public int hasKey = 0;
+
+
+    public Player(GamePanel gp, KeyHandler keyH) {
+        this.gp = gp;
+        this.keyH = keyH;
+
+        screenX = gp.screenWidth/2 - (gp.tileSize/2);
+        screenY = gp.screenHeight/2 - (gp.tileSize/2);
+
+        solidArea = new Rectangle();
+        solidArea.x = 8;
+        solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
+        solidArea.width = 32;
+        solidArea.height = 32;
+
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void setDefaultValues(){
+
+        worldX = gp.tileSize * 23;
+        worldY = gp.tileSize * 21;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage(){
+
+            up1 = setup("boy_up_1");
+            up2 = setup("boy_up_2");
+            down1 = setup("boy_down_1");
+            down2 = setup("boy_down_2");
+            left1 = setup("boy_left_1");
+            left2 = setup("boy_left_2");
+            right1 =  setup("boy_right_1");
+            right2 = setup("boy_right_2");
+
+    }
+
+    public BufferedImage setup(String imageName){
+        UtilityTool uTool = new UtilityTool();
+        BufferedImage image = null;
+
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/"+ imageName +".png")));
+            image = uTool.scaleImage(image,gp.tileSize,gp.tileSize);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return image;
+
+    }
+    public void update(){
+
+        if (keyH.upPressed  || keyH.downPressed  || keyH.leftPressed || keyH.rightPressed){
+            if(keyH.upPressed){
+                direction = "up";
+            }
+            else if (keyH.downPressed){
+                direction = "down";
+            }
+            else if (keyH.leftPressed){
+                direction = "left";
+            }
+            else {
+                direction = "right";
+            }
+            //Check tile collsion
+            collisionOn = false;
+            gp.coliChecker.checkTile(this);
+
+            // CHECK OBJECT COLLSION
+            int objIndex = gp.coliChecker.checkObject(this, true);
+            pickUpObject(objIndex);
+
+            //If Collison is false, player goes on
+            if(!collisionOn){
+                switch (direction) {
+                    case "up" -> worldY -= speed;
+                    case "down" -> worldY += speed;
+                    case "left" -> worldX -= speed;
+                    case "right" -> worldX += speed;
+                }
+            }
+
+            spriteCounter++;
+            if(spriteCounter > 10) {
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                }else if(spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
+
+
+    }
+
+    public void pickUpObject(int i){
+
+        if(i != 999){
+
+            String objectName = gp.obj[i].name;
+
+            switch (objectName){
+                case "Key":
+                    gp.playSE(1);
+                    hasKey++;
+                    gp.obj[i] = null;
+                    gp.ui.showMessage("Picked a Key!!");
+
+                    break;
+                case "Door":
+                    if(hasKey > 0){
+                        gp.playSE(3);
+                        gp.obj[i] = null;
+                        hasKey--;
+                        gp.ui.showMessage("You Opened the Door!!");
+                    }else{
+                        gp.ui.showMessage("You need key to open this door!");
+                    }
+                    break;
+                case "Boots":
+                    gp.playSE(2);
+                    speed += 1;
+                    gp.ui.showMessage("I am SPEED!!");
+                    gp.obj[i] = null;
+                    break;
+
+                case "Chest":
+                    gp.ui.gameFinished = true;
+                    gp.stopMusic();
+                    gp.playSE(4);
+                    break;
+            }
+        }
+    }
+
+
+    public void draw(Graphics2D g2){
+
+        BufferedImage image = null;
+        switch (direction) {
+            case "up" -> {
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+            }
+            case "down" -> {
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+            }
+            case "left" -> {
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
+            }
+            case "right" -> {
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
+            }
+        }
+
+        g2.drawImage(image, screenX, screenY,null);
+
+    }
+}
+
